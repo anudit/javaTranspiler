38280d1842a32f2df9203b1c693ce7be6ffd9771
Anudit Nagar <nagaranudit@gmail.com>
2019-01-08 20:18:45 +0530

    Add Argument Parser

--- transpile.py
+++ transpile.py
@@ -1,6 +1,8 @@
 import json
 import pprint
 import sys
+import os
+import argparse
 
 def returnWithTemplateCode(code, className):
     processed = ""
@@ -12,7 +14,7 @@ def returnWithTemplateCode(code, className):
             template = template.replace("/*<!--className-->*/",className)
             processed = template
     except IOError:
-        print("[File Error] Can't read to File.")
+        print("[File Error] Can't read to File.3")
     return processed
 
 def generateClassFiles(data):
@@ -22,7 +24,7 @@ def generateClassFiles(data):
             className = cell['source'][0][2:-3]
             end = len(cell['source'])
             for i in range(1,end):
-                if(i == 1)
+                if(i == 1):
                     code += cell['source'][i]
                 else:
                     code += "\t\t" + cell['source'][i]
@@ -33,12 +35,34 @@ def generateClassFiles(data):
                 with open("./"+ FOLDERNAME + "/" + className + ".java", "w") as file:  
                     file.write(returnWithTemplateCode(code, className))
             except IOError:
-                print("[File Error] Can't write to File.")
+                print("[File Error] Can't write to File.2")
 
-
-FILENAME = "test.ipynb"
+FILENAME = ""
 FOLDERNAME = "src"
-PKGNAME = "lab"
+PKGNAME = ""
+TEMPLATE = ""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-file',type=str, help="Specify File Name")
+parser.add_argument('-pkg',type=str, help="Specify Package Name")
+parser.add_argument('-template',type=str, help="Specify Template Address")
+args = parser.parse_args()
+
+if( type(args.file) != str ):
+    print("Specify File Name")
+    sys.exit(0)
+else:
+    FILENAME = args.file
+
+if( type(args.pkg) != str ):
+    print("Specify File Name")
+    sys.exit(0)
+else:   
+    PKGNAME = args.pkg
+
+
+if(os.path.exists(FOLDERNAME) != True):
+    os.mkdir(FOLDERNAME)
 
 try:
     with open(FILENAME, "r") as file:  
@@ -46,5 +70,5 @@ try:
         generateClassFiles(data)
         sys.exit(0)
 except IOError:
-    print("[File Error] Can't read to File.")
+    print("[File Error] Can't read File.1")
     sys.exit(0)
