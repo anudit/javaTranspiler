81dca44a2f43a5f75db40ee49ba9cd63f02d6827
Anudit Nagar <nagaranudit@gmail.com>
2019-01-08 19:51:32 +0530

    Update for Template Code Injection

--- transpile.py
+++ transpile.py
@@ -2,26 +2,43 @@ import json
 import pprint
 import sys
 
-FOLDERNAME = "src"
+def returnWithTemplateCode(code, className):
+    processed = ""
+    try:
+        with open("template.class", "r") as file:  
+            template = str(file.read())
+            template = template.replace("/*<!--code-->*/",code)
+            template = template.replace("/*<!--pkgName-->*/",PKGNAME)
+            template = template.replace("/*<!--className-->*/",className)
+            processed = template
+    except IOError:
+        print("[File Error] Can't read to File.")
+    return processed
 
 def generateClassFiles(data):
     for cell in data['cells']:
         if(cell['cell_type'] == 'code'):
             code = ""
             className = cell['source'][0][2:-3]
-            for line in cell['source']:
-                code+=line
-
-            print(className)
+            end = len(cell['source'])
+            for i in range(1,end):
+                if(i == 1)
+                    code += cell['source'][i]
+                else:
+                    code += "\t\t" + cell['source'][i]
+            className = className.replace(" ", "")
+            print("Processing Class : ", className)
 
             try:
                 with open("./"+ FOLDERNAME + "/" + className + ".java", "w") as file:  
-                    file.write(code)
+                    file.write(returnWithTemplateCode(code, className))
             except IOError:
-                print("[File Error] Can't write to File. -1")
+                print("[File Error] Can't write to File.")
 
 
-FILENAME = "Lab1.ipynb"
+FILENAME = "test.ipynb"
+FOLDERNAME = "src"
+PKGNAME = "lab"
 
 try:
     with open(FILENAME, "r") as file:  
@@ -29,5 +46,5 @@ try:
         generateClassFiles(data)
         sys.exit(0)
 except IOError:
-    print("[File Error] Can't read to File. -1")
+    print("[File Error] Can't read to File.")
     sys.exit(0)
